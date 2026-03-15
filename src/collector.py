0977b3c985c48f37d1939dd47d14e1729bfe8d1c
raz42852 <razderi55@gmail.com>
2026-03-16 01:40:50 +0200

    finish display stats

--- src/collector.py
+++ src/collector.py
@@ -6,33 +6,26 @@ def getCPUData():
     data = {}
     data['cores'] = psutil.cpu_count(logical=False)
     data['threads'] = psutil.cpu_count(logical=True)
-    data['cpu_per'] = psutil.cpu_percent(interval=1, percpu=True)
+    data['cpu_per'] = psutil.cpu_percent(interval=1, percpu=False)
+    data['cpu_per_per'] = psutil.cpu_percent(interval=1, percpu=True)
     data['freq'] = psutil.cpu_freq(percpu=True)
     return data
 
 def getMemory():
     # return dictionary with memory data
     virt_mem = psutil.virtual_memory()
-    data = {
-        "total" : bytes2human(virt_mem.total),
-        "available" : bytes2human(virt_mem.available),
-        "used" : bytes2human(virt_mem.used),
-        "percent" : bytes2human(virt_mem.percent),
-        "free" : bytes2human(virt_mem.free)
-    }
+    data = [
+        bytes2human(virt_mem.total),
+        bytes2human(virt_mem.available),
+        bytes2human(virt_mem.used),
+        f"{virt_mem.percent}%",
+        bytes2human(virt_mem.free)
+    ]
     return data
     
 def getDiskUsage():
     # return dictionary with disk usage
-    data = {
-        "device" : [],
-        "total" : [],
-        "used" : [],
-        "free" : [],
-        "use" : [],
-        "type" : [],
-        "mount" : [],
-    }
+    data = []
     for part in psutil.disk_partitions(all=False):
         if 'cdrom' in part.opts or not part.fstype:
             # skip cd-rom drives with no disk in it; they may raise
@@ -40,11 +33,13 @@ def getDiskUsage():
             # partition or just hang.
             continue
         usage = psutil.disk_usage(part.mountpoint)
-        data["device"].append(part.device)
-        data["total"].append(bytes2human(usage.total))
-        data["used"].append(bytes2human(usage.used))
-        data["free"].append(bytes2human(usage.free))
-        data["use"].append(int(usage.percent))
-        data["type"].append(part.fstype)
-        data["mount"].append(part.mountpoint)
+        data.append([
+            part.device,
+            f"{bytes2human(usage.total)}",
+            f"{bytes2human(usage.used)}",
+            f"{bytes2human(usage.free)}",
+            f"{int(usage.percent)}",
+            part.fstype,
+            part.mountpoint
+        ])
     return data
--- src/display.py
+++ src/display.py
@@ -1,6 +1,8 @@
 from rich.live import Live
 from rich.table import Table
 from rich.panel import Panel
+from rich.console import Console
+from rich.layout import Layout
 from rich import print
 import time
 import collector
@@ -8,18 +10,66 @@ import collector
 panel = Panel("System Monitoring CLI Tool", title="Welcome")
 print(panel)
 
-with Live(refresh_per_second=1) as live:
-    while True:
-        table = Table(title="CPU Panel")
-        table.add_column("Num Cores")
-        table.add_column("Num Threads")
-        cpu_data = collector.getCPUData()
-        for i in range(len(cpu_data['cpu_per'])):
-            table.add_column(f"Thread {i+1}")
-        for i in range(len(cpu_data['freq'])):
-            table.add_column(f"Frequency Thread {i+1}")
+console = Console()
+layout = Layout()
 
+layout.split_column(
+    Layout(name="upper"),
+    Layout(name="lower")
+)
+layout["lower"].split_row(
+    Layout(name="left"),
+    Layout(name="right"),
+)
+
+# Use Live to update the layout
+with Live(layout, refresh_per_second=1) as live:
+    while True:
+        cpu_data = collector.getCPUData()
+        cpu_table = Table(title="CPU Panel")
+
+        cpu_table.add_column("Num Cores")
+        cpu_table.add_column("Num Threads")
+        cpu_table.add_column("CPU Percentage")
+        cpu_row = [str(cpu_data['cores']),
+                   str(cpu_data['threads']),
+                   f"{str(cpu_data['cpu_per'])}%"]
+        
+        for i in range(len(cpu_data['cpu_per_per'])):
+            cpu_table.add_column(f"Thread {i+1}")
+            cpu_row.append(f"{cpu_data['cpu_per_per'][i]}%")
+        
+        for i in range(len(cpu_data['freq'])):
+            cpu_table.add_column(f"Frequency Thread {i+1}")
+            cpu_row.append(str(cpu_data['freq'][i].current))
+
+        cpu_table.add_row(*cpu_row)
+
+        memory_table = Table(title="Memory Panel")
+        memory_table.add_column("Total")
+        memory_table.add_column("Available")
+        memory_table.add_column("Used")
+        memory_table.add_column("Percent")
+        memory_table.add_column("Free")
+        memory_data = collector.getMemory()
+
+        memory_table.add_row(*memory_data)
+
+        disk_usage_table = Table(title="Disk Usage Panel")
+        disk_usage_table.add_column("Device")
+        disk_usage_table.add_column("Total")
+        disk_usage_table.add_column("Used")
+        disk_usage_table.add_column("Free")
+        disk_usage_table.add_column("Use")
+        disk_usage_table.add_column("Type")
+        disk_usage_table.add_column("Mount")
+        disk_usage_data = collector.getDiskUsage()
+        for row in disk_usage_data:
+            disk_usage_table.add_row(*row)
         # ... populate with data from collector ...
 
-        live.update(table)
+        layout["upper"].update(cpu_table)
+        layout["left"].update(memory_table)
+        layout["right"].update(disk_usage_table)
+
         time.sleep(2)
