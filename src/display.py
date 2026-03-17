45ef62dbfff8ab2216573943bd4d66db236d1581
raz42852 <razderi55@gmail.com>
2026-03-17 20:39:51 +0200

    adding log par & write into log json file

--- src/display.py
+++ src/display.py
@@ -7,11 +7,11 @@ import argparse
 from rich import print
 import time
 import collector
+from pathlib import Path
 import logger
 
 
-def get_cpu_table():
-    cpu_data = collector.getCPUData()
+def get_cpu_table(cpu_data):
     cpu_table = Table(title="CPU Panel")
     layout["upper"].size = 10
 
@@ -33,19 +33,18 @@ def get_cpu_table():
     cpu_table.add_row(*cpu_row)
     return cpu_table
 
-def get_memory_table():
+def get_memory_table(memory_data):
     memory_table = Table(title="Memory Panel")
     memory_table.add_column("Total")
     memory_table.add_column("Available")
     memory_table.add_column("Used")
     memory_table.add_column("Percent")
     memory_table.add_column("Free")
-    memory_data = collector.getMemory()
 
     memory_table.add_row(*memory_data)
     return memory_table
 
-def get_disk_usage_table():
+def get_disk_usage_table(disk_usage_data):
     disk_usage_table = Table(title="Disk Usage Panel")
     disk_usage_table.add_column("Device")
     disk_usage_table.add_column("Total")
@@ -54,7 +53,6 @@ def get_disk_usage_table():
     disk_usage_table.add_column("Use")
     disk_usage_table.add_column("Type")
     disk_usage_table.add_column("Mount")
-    disk_usage_data = collector.getDiskUsage()
     for row in disk_usage_data:
         disk_usage_table.add_row(*row)
     return disk_usage_table
@@ -62,11 +60,12 @@ def get_disk_usage_table():
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--interval', type=int, help="Enter an interval", default=2)
-# parser.add_argument("--log", type=dir)
+parser.add_argument("--log", type=str, help="Enter an path for logger file", default=2)
 args = parser.parse_args()
 
 interval = args.interval
-
+path = args.log
+print(path)
 
 panel = Panel("System Monitoring CLI Tool", title="Welcome")
 print(panel)
@@ -83,13 +82,19 @@ layout["lower"].split_row(
     Layout(name="right"),
 )
 
+logger_file = Path(path).joinpath("logger.json")
+logger.init_file(logger_file)
+
 # Use Live to update the layout
 with Live(layout, refresh_per_second=1) as live:
     while True:
-        cpu_table = get_cpu_table()
-        memory_table = get_memory_table()
-        disk_usage_table = get_disk_usage_table()
-
+        cpu_data = collector.getCPUData()
+        memory_data = collector.getMemory()
+        disk_usage_data = collector.getDiskUsage()
+        cpu_table = get_cpu_table(cpu_data)
+        memory_table = get_memory_table(memory_data)
+        disk_usage_table = get_disk_usage_table(disk_usage_data)
+        logger.save_json(cpu_data, memory_data, disk_usage_data, logger_file)
 
         layout["upper"].update(cpu_table)
         layout["left"].update(memory_table)
---
+++ src/logger.py
@@ -0,0 +1,56 @@
+import json
+
+def save_json(cpu_data, memory_data, disk_usage_data, path):
+    with open(path, 'r+') as file:
+        file_data = json.load(file)
+        file_data["CPU"].append(get_cpu_json(cpu_data))
+        file_data["Memory"].append(get_memory_json(memory_data))
+        file_data["Disk Usage"].append(get_disk_usage_json(disk_usage_data))
+        file.seek(0)
+        json.dump(file_data, file)
+
+def get_cpu_json(cpu_data):
+    dic_cpu = {}
+    dic_cpu['Num Cores'] = cpu_data['cores']
+    dic_cpu['Num Threads'] = cpu_data['threads']
+    dic_cpu['CPU Percentage'] = f"{str(cpu_data['cpu_per'])}%"
+    for i in range(len(cpu_data['cpu_per_per'])):
+        dic_cpu[f"Thread {i+1}"] = f"{cpu_data['cpu_per_per'][i]}%"
+    for i in range(len(cpu_data['freq'])):
+        dic_cpu[f"Frequency Thread {i+1}"] = cpu_data['freq'][i].current
+    return dic_cpu
+
+def get_memory_json(memory_data):
+    dic_memory = {}
+    dic_memory['Total'] = memory_data[0]
+    dic_memory['Available'] = memory_data[1]
+    dic_memory['Used'] = memory_data[2]
+    dic_memory['Percent'] = memory_data[3]
+    dic_memory['Free'] = memory_data[4]
+    return dic_memory
+
+def get_disk_usage_json(disk_usage_data):
+    all_disks = []
+    for disk in disk_usage_data:
+        dic_disk = {}
+        dic_disk['Device'] = disk[0]
+        dic_disk['Total'] = disk[1]
+        dic_disk['Used'] = disk[2]
+        dic_disk['Free'] = disk[3]
+        dic_disk['Use'] = disk[4]
+        dic_disk['Type'] = disk[5]
+        dic_disk['Mount'] = disk[6]
+        all_disks.append(dic_disk)
+    return all_disks
+
+def init_file(path):
+    if (not path.exists()):
+        open("logger.json", "x")
+        with open(path, 'w') as file:
+            file.write("""{ "CPU" : [], "Memory" : [], "Disk Usage" : []}""")
+    else:
+        with open(path, 'r') as file:
+            first_char = file.read(1)
+            if not first_char:
+                with open(path, 'w') as file:
+                    file.write("""{ "CPU" : [], "Memory" : [], "Disk Usage" : []}""")
